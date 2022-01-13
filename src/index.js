5c4cb50de46755ff9550447793cb580980d5eec8
flavio manzotti <manzottiflavio22@gmail.com>
2022-01-12 21:34:34 -0300

    post todos ok

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ app.use(express.json());
 function checksExistsUserAccount(request, response, next) {
   const {username}=request.headers;
 
-const user = users.find((user)=>user.username===username);
+const user = users.find(user=>user.username===username);
 
 if(!users){
   return response.status(400).json({error:"users not found"})
 }
-request.users=users;
+request.user=user;
 
 return next();
 }
@@ -27,31 +27,34 @@ return next();
 app.post('/users', (request, response) => {
   const {name,username}=request.body;
   
-  const user={ 
+  const usersAreadyExisty = users.find(user => user.username === username);
+
+  if(usersAreadyExisty){
+    return response.status(400).json({error:'usersAreadyExisty'});
+    }
+
+  const user=({ 
     id: uuidv4(), // precisa ser um uuid
     name, 
     username, 
     todos: []
-  }
-if(!user){
-  return response.status(400).json("{users does not existy}");
-  }else{
-    return response.status(201).json(user)
-  }
+  })
+ return response.status(201).json(user)
+  
   users.push(user)
 
-//const usersAreadyExisty=user.some((users)=>users.username===username);
+
 
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  const {username}=request.headers;
-  const {todos}=request.body;
-  const {title}=request.body;
+  const {user}=request;
+  const{todos}=request.body;
+
   if(!todos){
     return response.status(400).json({message:"todos not found"})
   }else{
-    return response.status(201).json(todos);
+    return response.status(201).json(user.todos);
   }
  
 });
@@ -59,24 +62,23 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   const {username}=request.headers;
   const{user}=request.body;
-  const {title}=request.body;
+  const {title, deadline}=request.body;
   
-
-  const todos={ 
-    id: uuidv4(), // precisa ser um uuid
+    const todos={ 
+    id: uuidv4(), 
     title,
     done: false, 
-    deadline:new Date('deadline'),
-    created_at:new date(2022-10-14)
+    deadline: new Date(deadline), 
+    created_at: new Date()
   }
 
-  //todos.push(user)
+  
 if(!todos){
   return response.status(400).send({message:"error:todos not create"})
 }else{
   return response.status(201).json(todos)
 }
- date.push(todos);
+ //user.todos.push(todos)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
